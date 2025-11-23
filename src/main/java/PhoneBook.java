43eeb3f85cc62d27b208eb87b177a2e2119dcd26
Maksim <maks228sh@gmail.com>
2025-11-23 19:09:57 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,9 @@
+import java.util.Map;
+
 public class PhoneBook {
+
+    public int addNumber(Map<String, Integer> number) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    void addNumberTestOne() {
+
+        Map<String, Integer> number = Map.of("Маша", 232_013,
+                "Влад", 232_021,
+                "Макс", 232_732);
+
+        int result = phoneBook.addNumber("Олег", 232_032);
+
+        assertEquals(4, result, "3 + 1 должно быть 4");
+
+    }
+
+    @Test
+    void addNumberTestTwo() {
+
+        Map<String, Integer> number = Map.of("Маша", 232_013,
+                "Влад", 232_021,
+                "Макс", 232_732);
+
+        int result = phoneBook.addNumber("Макс", 232_032);
+
+        assertEquals(3, result, "Имена повторяются, ответ 3");
+
+    }
 
 }
